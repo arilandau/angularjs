e1ddffbc17fd61fbd375994e99eb1a4c9ef12057
ARILANDAU <ariellelandau360@gmail.com>
2020-10-18 20:37:33 -0400

    skeleton form

--- public_html/js/controllers.js
+++ public_html/js/controllers.js
@@ -1,14 +1,25 @@
 var helloWorldControllers = angular.module('helloWorldControllers', []);
 
-helloWorldControllers.controller('MainCtrl', ['$scope', '$location', '$http', 
+helloWorldControllers.controller('MainCtrl', ['$scope', '$location', '$http',
     function MainCtrl($scope, $location, $http) {
         $scope.message = 'Hello World';
+        $scope.master = {};
+
+        $scope.submit = function(user) {
+          $scope.master = angular.copy(user);
+          console.log("final user", $scope.master)
+        };
+
+        $scope.reset = function() {
+          $scope.user = angular.copy($scope.master);
+        };
+
+        $scope.reset();
+
     }]);
 
 
-helloWorldControllers.controller('ShowCtrl', ['$scope', '$location', '$http', 
+helloWorldControllers.controller('ShowCtrl', ['$scope', '$location', '$http',
     function ShowCtrl($scope, $location, $http) {
         $scope.message = 'Show World';
     }]);
-
-
